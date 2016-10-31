abe2e6eaf5d9c6aaadbcac84788ca7a400b7ce19
Nolan Zandi <nolan@zandi.com>
2016-10-31 00:17:41 -0700

    add fields to user forms

--- app/overrides/edit_user_create.rb
+++ app/overrides/edit_user_create.rb
@@ -17,3 +17,37 @@ Deface::Override.new(
           </div>
 "
 )
+
+Deface::Override.new(
+    :virtual_path   => "spree/shared/_user_form",
+    :name           => "edit_signup_form",
+    :insert_after  => "erb[loud]:contains('email_field :email')",
+    :text           => "
+          <p>
+                <%= f.label :company_name, Spree.t(:company_name) %><br />
+                <%= f.text_field :company_name, class: 'title' %>
+          </p>
+          <p>
+                <%= f.label :first_name, Spree.t(:first_name) %><br />
+                <%= f.text_field :first_name, class: 'title' %>
+          </p>
+          <p>
+                <%= f.label :last_name, Spree.t(:last_name) %><br />
+                <%= f.text_field :last_name, class: 'title' %>
+          </p>
+"
+)
+
+Deface::Override.new(
+    :virtual_path   => "spree/users/show",
+    :name           => "edit_signup_show",
+    :insert_after  => "erb[loud]:contains('spree.edit_account_path')",
+    :text           => "
+                        <dt><%= Spree.t(:company_name) %></dt>
+                          <dd><%= @user.company_name %> (<%= link_to Spree.t(:edit), spree.edit_account_path %>)</dd>
+                        <dt><%= Spree.t(:first_name) %></dt>
+                          <dd><%= @user.first_name %> (<%= link_to Spree.t(:edit), spree.edit_account_path %>)</dd>
+                        <dt><%= Spree.t(:last_name) %></dt>
+                          <dd><%= @user.last_name %> (<%= link_to Spree.t(:edit), spree.edit_account_path %>)</dd>
+"
+)
