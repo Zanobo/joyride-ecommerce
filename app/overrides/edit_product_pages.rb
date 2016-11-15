a178a702e7e83b8c70e2dfd3cbe3c6adedaca3f9
Nolan Zandi <nolan@zandi.com>
2016-11-14 23:31:06 -0800

    updates to product modal

--- app/overrides/edit_product_pages.rb
+++ app/overrides/edit_product_pages.rb
@@ -3,9 +3,16 @@ Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :replace_contents => ".product-image") do
               ' <%= link_to image_tag(product.display_image.attachment(:small), itemprop: "image"), url,
                 {:remote => true, "data-toggle" =>  "modal", "data-target" => "#myModal"} %>
-                <%= product.name%>
                 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 
                 </div>
 '
 end
+
+Deface::Override.new(:virtual_path => "spree/home/index",
+                     :name => "embed_product_modal",
+                     :insert_after  => "[data-hook='homepage_products']",
+                     :text           => '
+                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                </div>
+')
