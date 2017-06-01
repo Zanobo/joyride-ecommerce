af4bcbf884da6e6eec37ec43ca2dac0a8d206052
lcostantini <costantinileandro1@gmail.com>
2017-06-01 16:02:53 -0300

    Add import for customer with restriction group

--- lib/acumatica/import.rb
+++ lib/acumatica/import.rb
@@ -81,9 +81,18 @@ module Acumatica
 
   def get_items_by_restriction_groups group
     filter = "GroupName eq '#{group}' and Active eq true and Included eq true"
-    custom_url = "#{Acumatica::API}/ItemRestrictionGroups?$filter=#{filter}"
-    item_ids = get(custom_url, timeout: 360).map { |g| g['InventoryCD']['value'] }
+    items_url = "#{Acumatica::API}/ItemRestrictionGroups?$filter=#{filter}"
+    item_ids = JSON.parse(get(items_url, timeout: 360).body.force_encoding('UTF-8'))
+      .map { |c| c['InventoryCD']['value'] }
     Spree::Variant.where(id: item_ids)
+  end
+
+  def get_customer_by_restriction_groups group
+    filter = "GroupName eq '#{group}' and Active eq true and Included eq true"
+    customer_url = "#{Acumatica::API}/CustomerRestrictionGroups?$filter=#{filter}"
+    customer_ids = JSON.parse(get(customer_url, timeout: 360).body.force_encoding('UTF-8'))
+      .map { |c| c['CustomerID']['value'] }
+    Spree::User.where(id: customer_ids)
   end
 
   def parse_cookies cookies
